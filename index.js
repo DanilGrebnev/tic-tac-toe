17758dadaa90aa73764de89ad1790cd40976d553
DanilGrebnev <93824242+DanilGrebnev@users.noreply.github.com>
2022-09-12 20:31:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ document.addEventListener('DOMContentLoaded', main)
 function main() {
     const get = selector => document.querySelector(selector)
     const getAll = selector => document.querySelectorAll(selector)
-    const BTN_RES = get('#res')
-    const BTN_START = get('#start')
+    const getRandom = (a) => Math.floor(Math.random() * a.length)
+    const RESTART = get('#res')
+    const START = get('#start')
     const CONTAINER = get('.container')
     const SELECT = get('select')
     const MENU = get('#menu')
-    const URL = "https://cdn-icons-png.flaticon.com/512/656/656857.png"
+    const X = "https://cdn-icons-png.flaticon.com/512/656/656857.png"
 
-    let count = 0
-    let player = true
-    let dual = false
-    let endGame = false
-    let win1 = false
-    let win2 = false
+    const state = {
+        count: 0,
+        player: true,
+        dual: true,
+        endGame: false,
+        win1: false,
+        win2: false
+    }
+
+    let o = JSON.parse(JSON.stringify(state))
+
+    START.addEventListener('click', startGame)
+    RESTART.addEventListener('click', restart)
 
     //Создание поля
     for (let i = 1; i <= 9; i++) {
@@ -24,172 +32,124 @@ function main() {
         `
     }
 
-    BTN_START.onclick = startGame
-    BTN_RES.onclick = restart
-
+    //Начало игры
     function startGame() {
-        const value = SELECT.value
-        dual = value === "people" ? true : false
+        if (SELECT.value === "bot") o.dual = false
+
         CONTAINER.addEventListener('click', step)
         MENU.style.display = "none"
-        BTN_RES.style.display = "block"
+        RESTART.style.display = "block"
     }
 
-    // 
+    // Условие шага 
     async function step(e) {
-        if (e.target.className !== "item") return
-
-        count++
-        e.target.classList.add('active')
-        addElem(e, player)
+        const event = e.target
+        if (event.className !== "item") return
+        o.count++
+        event.classList.add('active')
+        addElem(e, o.player)
         check()
-        player = !player
+        o.player = !o.player
 
         //Если условия соблюдены, то бот сделает шаг
-        if (!dual && count !== 9 && !endGame) {
+        if (!o.dual && o.count !== 9 && !o.endGame) {
+            CONTAINER.removeEventListener('click', step)
             await new Promise(resolve => {
                 setTimeout(() => resolve(bot()), 1000)
             })
             check()
+            CONTAINER.addEventListener('click', step)
         }
 
-        if (win1) setTimeout(alert, 500, "Победил игрок 1")
-        if (win2) setTimeout(alert, 500, "Победил игрок 2")
+        o.win1 && setTimeout(alert, 500, "Победил игрок 1")
+        o.win2 && setTimeout(alert, 500, "Победил игрок 2")
     }
 
     //Бот
     function bot() {
         //Бот случайно выбирает свободную ячейку и ходит в неё
-        count++
-        const arr = [...getAll(".item")]
-            .filter(el => !el.classList.contains('active'))
-
-        const random = getRandom(arr)
-        arr[random].classList.add('active')
-        arr[random].innerHTML = `<img class="x" src="${URL}">`
-        player = !player
-    }
-
-    function getRandom(arr) {
-        let a = Math.floor(Math.random() * arr.length)
-        return a
+        o.count++
+        const a = [...getAll(".item")].filter(el => !el.classList.contains('active'))
+        const rand = getRandom(a)
+        a[rand].classList.add('active')
+        a[rand].innerHTML = `<img class="x" src="${X}">`
+        o.player = !o.player
     }
 
     //Добавляет Крест или Круг в зависимости от очереди 
-    function addElem(e, player) {
-        if (player) {
-            e.target.innerHTML = `<div class ="circle"></div>`
-        } else {
-            e.target.innerHTML = `<img class="x" src="${URL}">`
-        }
+    function addElem(e, p) {
+        e.target.innerHTML = p ? '<div class ="circle"></div>' : `<img class="x" src="${X}">`
     }
 
-    //Условие выбора победителя
-    function choiceWin(a, p1) {
-        if (every(a, "circle")) {
-            a.forEach(el => el.classList.add(p1))
-            winner(1)
-            return
-        } else if (every(a, "x")) {
-            a.forEach(el => el.classList.add(p1))
-            winner(2)
-            return
-        }
+    //Условие выбора победившего игрока
+    function choiceWin(a, className) {
+        (every(a, "circle") || every(a, "x")) && a.forEach(el => el.classList.add(className))
+        every(a, "circle") && winner(1)
+        every(a, "x") && winner(2)
     }
 
-    //Проверка строк
-    function row(p1, p2) {
+    //Проверка строк и колонок
+    function rowCol(start, end, step = 1) {
         let a = []
-        for (let i = p1; i <= p2; i++) {
+        for (let i = start; i <= end; i += step) {
             a.push(get(`.item[data-number="${i}"]`))
         }
-        choiceWin(a, 'line')
-    }
-
-    //Проверка колонок
-    function col(p1, p2) {
-        let a = []
-        for (let i = p1; i <= p2; i += 3) {
-            a.push(get(`.item[data-number="${i}"]`))
-        }
-        choiceWin(a, "col")
+        choiceWin(a, step === 1 ? "line" : "col")
     }
 
     //Проверка диагоналей
-    function diag(p1, p2, p3) {
+    function diag(start, end, step = "r") {
         let a = []
-        let c = p3 === "l" ? "l" : "r"
-        let step = p3 === "l" ? 4 : 2
-
-        for (let i = p1; i <= p2; i += step) {
+        for (let i = start; i <= end; i += step === "l" ? 4 : 2) {
             a.push(get(`.item[data-number="${i}"]`))
         }
-
-        choiceWin(a, c)
+        choiceWin(a, step)
     }
 
     //Осуществляет проверку колонок, строк, диагоналей
     function check() {
-        if (count === 9 && !endGame) {
-            winner(3)
-            return
-        }
-
-        row(1, 3)
-        row(4, 6)
-        row(7, 9)
-        col(1, 7)
-        col(2, 8)
-        col(3, 9)
+        if (o.count === 9 && !o.endGame) return winner(3)
+        //Проверка строк
+        rowCol(1, 3)
+        rowCol(4, 6)
+        rowCol(7, 9)
+        //Проверка колонок
+        rowCol(1, 7, 3)
+        rowCol(2, 8, 3)
+        rowCol(3, 9, 3)
+        //Проверка диагоналей
         diag(1, 9, "l")
         diag(3, 7)
     }
 
     //Вывода на экран победителя
     function winner(p) {
-        if (p === 1) {
-            win1 = true
-            endGame = true
-            BTN_RES.style.display = "block"
-            return
-        } else if (p === 2) {
-            win2 = true
-            endGame = true
-            BTN_RES.style.display = "block"
-            return
-        } else {
-            setTimeout(alert, 500, "Ничья")
-            endGame = true
-            BTN_RES.style.display = "block"
-            return
+        if (p === 1 || p === 2 || p === 3) {
+            RESTART.style.display = "block"
+            o.endGame = true
         }
 
-    }
-    //Проврека наличия в массиве всех элементов с одним классом 
-    function every(a, selector) {
-        return a.every(el => {
-            if (el.firstChild) {
-                if (el.firstChild.className === selector) {
-                    return true
-                }
-            }
-        })
+        p === 1 ? o.win1 = true : null
+        p === 2 ? o.win2 = true : null
+        p === 3 ? setTimeout(alert, 500, "Ничья") : null
     }
 
+    //Проврека наличия в массиве всех элементов с одним классом 
+    const every = (a, selector) => a.every(el => {
+        if (el.firstChild) {
+            if (el.firstChild.className === selector) return true
+        }
+    })
+
     function restart(e) {
-        count = 0
-        const ITEMS = document.querySelectorAll('.item')
-        player = true
-        ITEMS.forEach(el => {
+        o = JSON.parse(JSON.stringify(state))
+        getAll('.item').forEach(el => {
             el.className = "item"
             el.innerHTML = ""
         })
         e.target.style.display = "none"
-        endGame = false
         MENU.style.display = "block"
-        CONTAINER.removeEventListener('click', main)
-        win1 = false
-        win2 = false
+        CONTAINER.removeEventListener('click', step)
     }
 
 }
